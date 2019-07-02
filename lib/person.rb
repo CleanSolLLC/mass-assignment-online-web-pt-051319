ae4322c7ca1f97882c44e89bbaa898daade4c4e1
Mark May <mamark90@gmail.com>
2019-07-02 02:08:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,9 @@ class Person
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-   attributes.each {|key, value| self.send(("#{key}="), value)}
-   binding.pry
+   attributes.each do |key, value|
+     binding.pry
+      self.send(("#{key}="), value)
+    end
  end
 end
